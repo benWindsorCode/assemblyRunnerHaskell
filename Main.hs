f2ebcd4c774041c50fa1c6495898b2ae78c33a12
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-11-26 15:24:14 +0000

    Start implementation of instructions

--- Main.hs
+++ Main.hs
@@ -8,14 +8,44 @@ data Register = Register {
     out :: Int
 } deriving (Show)
 
--- define data type to represent the registers
+type Label = String 
+type Instruction = String
+type Source = String
+type Destination = String
+data Command = Label Label| TwoParam Instruction Source Destination | OneParam Instruction Source
 -- define function that takes instruction and current state and returns next state
 
-executeCommand :: Register -> String -> Register
-executeCommand reg command = reg
+-- Split line into words then pass into parseWords
+-- parseLine :: String -> Command
+-- parseLine = "Not implemented"
+
+parseWords :: [String] -> Command
+parseWords (a:b:[]) = OneParam a b
+parseWords (a:b:c:[]) = TwoParam a b c
+
+
+executeCommand :: Register -> Command -> Register
+executeCommand reg (Label label) = reg
+executeCommand reg (TwoParam instruction source destination) = mov reg source destination
+executeCommand reg (OneParam instruction source) 
+    | instruction == "ADD" = add reg (read source :: Int)
+
+add :: Register -> Int -> Register
+add (Register acc bak dat input out) val = Register (acc + val) bak dat input out
+
+accessRegister :: Register -> String -> Int
+accessRegister (Register acc bak dat input out) "ACC" = acc
+accessRegister (Register acc bak dat input out) "BAK" = bak
+accessRegister (Register acc bak dat input out) "DAT" = dat
+accessRegister (Register acc bak dat input out) "INPUT" = input
+accessRegister (Register acc bak dat input out) "OUT" = out
+
+mov :: Register -> String -> String -> Register
+mov (Register acc bak dat input out) "IN" "DAT" = Register acc bak input input out
 
 main :: IO ()
 main = do
-    putStrLn $ show $ executeCommand reg "ga"
+    putStrLn $ show $ executeCommand reg (OneParam "ADD" "5")
+    putStrLn $ show $ add reg 5
     where 
-        reg = Register 1 1 1 1 1
+        reg = Register 0 0 0 0 0
